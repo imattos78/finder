697601e3bff469f5721741012e002db3a44e7c54
ignacio <nachomattos78@gmail.com>
2019-12-12 23:41:29 +0000

    header image and favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import logo from "../images/LogoMakr_8vj7Vz.png"
 
 class Header extends React.Component {
     render () {
         return (
             <div>
-                <h1>Finder</h1>
+                 <img src={logo} alt="Logo" />
                 <h5>Starter header for app</h5>
             </div>
         );
