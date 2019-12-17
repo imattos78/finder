e5505dd11a74ce7561fd5e9f8d1eb15f2c8f1590
AnnJBarr <annbarr@hotmail.com>
2019-12-17 11:43:03 +0000

    styling added for add pharmacy and list pharmacies

--- src/components/AddPharmacy.js
+++ src/components/AddPharmacy.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSyringe, faTruck, faLaptopMedical, faCity, faComment } from '@fortawesome/free-solid-svg-icons';
+import w3wlogo from "../images/w3w_logo.png"
 
 class AddPharmacy extends React.Component {
 
@@ -17,7 +20,7 @@ class AddPharmacy extends React.Component {
                                         <div className="col-12 col-lg-8">
                                             <div className="input-group ">
                                                 <div className="form-group col-12 col-lg-9">
-                                                    <label htmlFor="examplew3wLocation">Location</label>
+                                                    <label htmlFor="examplew3wLocation"><img className="card-title" width="30 px" src={w3wlogo} alt="w3wLogo" /> Location</label>
                                                     <input
                                                         type="text"
                                                         className="form-control"
@@ -26,36 +29,31 @@ class AddPharmacy extends React.Component {
                                                     ></input>
                                                 </div>
                                                 <div className="form-group col-12 col-lg-9">
-                                                    <label htmlFor="exampleTown">Town</label>
+                                                    <label htmlFor="exampleTown"><FontAwesomeIcon icon={faCity} /> Town</label>
                                                     <input type="text" className="form-control" id="exampleTown" placeholder="Enter Town"></input>
                                                 </div>
-                                                <div className="input-group">
-                                                    <div className="input-group-prepend">
-                                                        <span className="input-group-text">Comments</span>
-                                                    </div>
-                                                    <textarea className="form-control" aria-label="comments"></textarea>
-                                                </div>
-                                                {/* <div className="form-group">
-                    <label for="exampleInputTown">Town</label>
-                    <input type="text" className="form-control" id="exampleInputTown" placeholder="Password"></input>
-                </div> */}
-                                                <p></p>
+
                                                 <div className="form-group col-4 form-check form-check-inline">
                                                     <input type="checkbox" className="form-check-input" id="lateOpening"></input>
-                                                    <label className="form-check-label" htmlFor="lateOpening">Late Opening</label>
+                                                    <label className="form-check-label" htmlFor="lateOpening">Late Opening <FontAwesomeIcon icon={faMoon} /></label>
                                                 </div>
                                                 <div className="form-group col-4 form-check form-check-inline">
                                                     <input type="checkbox" className="form-check-input" id="vaccs"></input>
-                                                    <label className="form-check-label" htmlFor="vaccs">Vaccinations</label>
+                                                    <label className="form-check-label" htmlFor="vaccs">Vaccinations <FontAwesomeIcon icon={faSyringe} /></label>
                                                 </div>
                                                 <div className="form-group col-4 form-check form-check-inline">
                                                     <input type="checkbox" className="form-check-input" id="delivery"></input>
-                                                    <label className="form-check-label" htmlFor="delivery">Delivery</label>
+                                                    <label className="form-check-label" htmlFor="delivery">Delivery <FontAwesomeIcon icon={faTruck} /></label>
                                                 </div>
                                                 <div className="form-group col-4 form-check form-check-inline">
                                                     <input type="checkbox" className="form-check-input" id="eScript"></input>
-                                                    <label className="form-check-label" htmlFor="eScript">e-Prescriptions accepted</label>
+                                                    <label className="form-check-label" htmlFor="eScript">e-Prescriptions accepted <FontAwesomeIcon icon={faLaptopMedical} /></label>
                                                 </div>
+                                                <div className="form-group col-12 col-lg-9">
+                                                    <label htmlFor="exampleComments"><FontAwesomeIcon icon={faComment} /> Comments</label>
+                                                    <input type="textarea" className="form-control" id="exampleComments" placeholder="Add your comments"></input>
+                                                </div>
+                                            
                                                 <div className="col-12">
                                                     <button type="submit" className="add">Submit</button>
                                                 </div>
--- src/components/PharmaciesList.js
+++ src/components/PharmaciesList.js
@@ -1,48 +1,57 @@
 import React from "react";
 import PharmacyItem from "./PharmacyItem"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSyringe, faTruck, faLaptopMedical, faMapMarked } from '@fortawesome/free-solid-svg-icons';
+import w3wlogo from "../images/w3w_logo.png"
+
 
 class PharmaciesList extends React.Component {
     render() {
         return (
 
-            <div className="card text-white bg-white mb-3" >
-                <div className="card-header">
-                    <h5>Pharmacies Found!</h5>
-                </div>
-                <div className="card-body">
-                    <h5 className="card-title">Primary card title</h5>
-                    <div className="card-text">
-                        <div className="row taskRow">
-                            <div className="col-2 col-md-1">
-                                <h5>w3w</h5>
-                            </div>
+            <div id="list-pharmacy" className="row appRow">
+                <div className="mx-auto col-12 col-lg-9">
+                    <div className="card bg-white mb-3" >
 
-                            <div className="col-10 col-md-7 ">
-                                <h5>Town</h5>
+                        <div className="card-header bg-transparent">
+                            <h5>Pharmacies Found!</h5>
+                            <div className="row taskRow">
+                                <div className="col-2 col-md-1">
+                                    <h5><FontAwesomeIcon icon={faMapMarked} /></h5>
+                                </div>
+
+                                <div className="col-8 col-md-5 ">
+                                    <h6>Town</h6>
+                                </div>
+                                <div className="col-2 col-md-1">
+                                    <h5><FontAwesomeIcon icon={faMoon} /></h5>
+                                </div>
+                                <div className="col-2 col-md-1">
+                                    <h5><FontAwesomeIcon icon={faSyringe} /></h5>
+                                </div>
+                                <div className="col-2 col-md-1">
+                                    <h5><FontAwesomeIcon icon={faTruck} /></h5>
+                                </div>
+                                <div className="col-2 col-md-1">
+                                    <h5><FontAwesomeIcon icon={faLaptopMedical} /></h5>
+                                </div>
                             </div>
-                            <div className="col-2 col-md-1">
-                                <h5>L</h5>
-                            </div>
-                            <div className="col-2 col-md-1">
-                                <h5>V</h5>
-                            </div>
-                            <div className="col-2 col-md-1">
-                                <h5>D</h5>
-                            </div>
-                            <div className="col-2 col-md-1">
-                                <h5>E</h5>
-                            </div>
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">Primary card title</h5>
+                            <div className="card-text">
 
 
-                            <h5><PharmacyItem text="pharmacy1" />Some quick example text to build on the card title and make up the bulk of the card's content.</h5></div>
+
+                                <h5><PharmacyItem text="pharmacy1" />Some quick example text to build on the card title and make up the bulk of the card's content.</h5></div>
+                        </div>
                     </div>
 
 
-
                 </div>
 
-            </div>
 
+            </div>
 
         );
     }
