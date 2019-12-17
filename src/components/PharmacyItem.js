901d843b17f3cc33ff01fb7f9e3ab921948e7cbc
AnnJBarr <annbarr@hotmail.com>
2019-12-17 14:37:52 +0000

    html links to open in new tab added to pharmacies list

--- src/components/PharmacyItem.js
+++ src/components/PharmacyItem.js
@@ -1,19 +1,29 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faRedoAlt } from '@fortawesome/free-solid-svg-icons';
+import w3wlogo from "../images/w3w_logo.png"
 class PharmacyItem extends React.Component {
     render() {
         console.log(this.props.location)
+        const link = "https://what3words.com/" + this.props.location
+        console.log(link)
         return (
             <div>
                 <div className="row pb-3 mb-3">
-                    <h5 className="col-2 col-md-1">{this.props.location}</h5> 
-                    <h5 className="col-8 col-md-5">{this.props.town}</h5>
+                    <h5 className="col-2 col-md-1"><a href={link} target="_blank" rel="noopener noreferrer"><img width="30 px" src={w3wlogo} alt="w3wLogo" /></a></h5>
+                    <h5 className="col-5 col-md-5">{this.props.town}</h5>
                     <h5 className="col-2 col-md-1">{this.props.late}</h5>
                     <h5 className="col-2 col-md-1">{this.props.vaccine}</h5>
                     <h5 className="col-2 col-md-1">{this.props.delivery}</h5>
                     <h5 className="col-2 col-md-1">{this.props.e_pres}</h5>
+                    <div className="col-1">
+                        <button className="btn"  ><FontAwesomeIcon icon={faRedoAlt} /></button>
+                    </div>
+                    <div className="col-1">
+                        <button className="btn"  ><FontAwesomeIcon icon={faTrashAlt} /></button>
+                    </div>
                 </div>
-            </div>
+            </div >
         );
     }
 }
