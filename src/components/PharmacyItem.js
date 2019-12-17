62606cb355833ed3832f8c248fe3ffa74695b74f
AnnJBarr <annbarr@hotmail.com>
2019-12-17 14:57:41 +0000

    0s and 1s replaced with icons in pharmacies list

--- src/components/PharmacyItem.js
+++ src/components/PharmacyItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt, faRedoAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, faRedoAlt, faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import w3wlogo from "../images/w3w_logo.png"
 class PharmacyItem extends React.Component {
     render() {
@@ -12,10 +12,10 @@ class PharmacyItem extends React.Component {
                 <div className="row pb-3 mb-3">
                     <h5 className="col-2 col-md-1"><a href={link} target="_blank" rel="noopener noreferrer"><img width="30 px" src={w3wlogo} alt="w3wLogo" /></a></h5>
                     <h5 className="col-5 col-md-5">{this.props.town}</h5>
-                    <h5 className="col-2 col-md-1">{this.props.late}</h5>
-                    <h5 className="col-2 col-md-1">{this.props.vaccine}</h5>
-                    <h5 className="col-2 col-md-1">{this.props.delivery}</h5>
-                    <h5 className="col-2 col-md-1">{this.props.e_pres}</h5>
+                    <h5 className="col-2 col-md-1">{this.props.late === 1 ? <FontAwesomeIcon icon={faCheckCircle} /> : <FontAwesomeIcon icon={faTimesCircle} />}</h5>
+                    <h5 className="col-2 col-md-1">{this.props.vaccine === 1 ? <FontAwesomeIcon icon={faCheckCircle} /> : <FontAwesomeIcon icon={faTimesCircle} />}</h5>
+                    <h5 className="col-2 col-md-1">{this.props.delivery === 1 ? <FontAwesomeIcon icon={faCheckCircle} /> : <FontAwesomeIcon icon={faTimesCircle} />}</h5>
+                    <h5 className="col-2 col-md-1">{this.props.e_pres === 1 ? <FontAwesomeIcon icon={faCheckCircle} /> : <FontAwesomeIcon icon={faTimesCircle} />}</h5>
                     <div className="col-1">
                         <button className="btn"  ><FontAwesomeIcon icon={faRedoAlt} /></button>
                     </div>
