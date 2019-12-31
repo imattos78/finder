a91f66c825e1b15ae072af391288910c949d976e
AnnJBarr <annbarr@hotmail.com>
2019-12-31 10:25:58 +0000

    code tidy for PharmaciesList

--- src/components/PharmaciesList.js
+++ src/components/PharmaciesList.js
@@ -8,7 +8,6 @@ import w3wlogo from "../images/w3w_logo.png";
 class PharmaciesList extends React.Component {
     render() {
         const pharmacies = this.props.pharmacies
-        console.log(pharmacies)
 
         return (
 
@@ -17,7 +16,7 @@ class PharmaciesList extends React.Component {
                     <div className="card bg-white mb-2" >
 
                         <div className="card-header bg-transparent">
-                                                       <div className="row">
+                            <div className="row">
                                 <div className="col-9 col-md-6">
                                     <h5>Pharmacies Found! <img src={pharmFind} alt="icon magnifier with pharmacy symbol" width="30 px" /></h5>
                                 </div>
@@ -32,39 +31,27 @@ class PharmaciesList extends React.Component {
                                 <div className="col-10 col-md-3 ">
                                     <h5>Town</h5>
                                 </div>
-                                
+
                                 <div className="col-8 col-md-4 d-flex justify-content-around">
                                     <h5><FontAwesomeIcon icon={faMoon} /></h5>
-                              
                                     <h5><FontAwesomeIcon icon={faSyringe} /></h5>
-                              
                                     <h5><FontAwesomeIcon icon={faTruck} /></h5>
-                               
                                     <h5><FontAwesomeIcon icon={faLaptopMedical} /></h5>
                                 </div>
                             </div>
 
                         </div>
                         <div className="card-body">
-                            {/* <h5 className="card-title">Primary card title</h5> */}
                             <div className="card-text">
 
                                 {pharmacies.map(pharmacy => {
                                     return <PharmacyItem deleteItemFunc={this.props.deleteItemFunc} updateItemFunc={this.props.updateItemFunc} location={pharmacy.location} town={pharmacy.town} key={pharmacy.id} id={pharmacy.id} late={pharmacy.late} vaccine={pharmacy.vaccine} delivery={pharmacy.delivery} e_pres={pharmacy.e_pres} date={pharmacy.date} />
                                 })}
                             </div>
-                            {/* { id: 1, location: "still.glory.blur", town: "Cheadle", late: 1, vaccine: 0, delivery: 0, e_pres:1, date: "2019-10-15"}, */}
-
-                            {/* <h5><PharmacyItem pharmacies={pharmacies} text="pharmacy 1" />Some quick example text to build on the card title and make up the bulk of the card's content.</h5></div> */}
                         </div>
                     </div>
-
-
                 </div>
-
-
             </div>
-
         );
     }
 }
