b69366a484442d15298357c8d694e977f881a0df
AnnJBarr <annbarr@hotmail.com>
2020-01-05 17:06:18 +0000

    Updates

---
+++ precache-manifest.9e394ec9a851c6f60143cb8213d1592a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca1a9626a5895252f0085d03f8e4591",
+    "url": "/finder/index.html"
+  },
+  {
+    "revision": "72010e765e188bed61cf",
+    "url": "/finder/static/css/main.44c11bd0.chunk.css"
+  },
+  {
+    "revision": "0cfad882dc8720eb247e",
+    "url": "/finder/static/js/2.3d8502cb.chunk.js"
+  },
+  {
+    "revision": "1214f0350405497c3c6a2373448e27dd",
+    "url": "/finder/static/js/2.3d8502cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72010e765e188bed61cf",
+    "url": "/finder/static/js/main.940bbc1c.chunk.js"
+  },
+  {
+    "revision": "f6dec9340a4eee53b787",
+    "url": "/finder/static/js/runtime-main.f6c36cf8.js"
+  },
+  {
+    "revision": "11b74e524156af587a0565f832bef5a4",
+    "url": "/finder/static/media/LogoMakr_0bAO9b.11b74e52.png"
+  },
+  {
+    "revision": "2c5c520f32659dcce64c6980e2337453",
+    "url": "/finder/static/media/LogoMakr_1cvDUu.2c5c520f.png"
+  },
+  {
+    "revision": "718aa4cd94e506a5acc5830e06a38058",
+    "url": "/finder/static/media/LogoMakr_2XNkij.718aa4cd.png"
+  },
+  {
+    "revision": "c63634c9103a8450a75f3d87b1d07fea",
+    "url": "/finder/static/media/LogoMakr_8vj7Vz.c63634c9.png"
+  },
+  {
+    "revision": "e7595eb1ad3dc2d8a8dec989c85eec98",
+    "url": "/finder/static/media/team-logo.e7595eb1.svg"
+  }
+]);
